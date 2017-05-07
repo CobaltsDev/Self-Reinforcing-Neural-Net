26337eb1ed337097a5421821ddb3d9b1711988af
a_Sharp_a <Izzy@DESKTOP-1FLS1BQ>
2017-05-07 10:35:01 -0700

    Almost done!

--- testnn/cstest/Program.cs
+++ testnn/cstest/Program.cs
@@ -10,6 +10,7 @@ namespace cstest
     {
         static Synapse[] Synapses;
         static int[] Inputs;
+        static int[] Outputs = new int[]{ 1,0,1 };
         class Predict
         {
             public int p()
@@ -28,11 +29,17 @@ namespace cstest
                     return 0;
                 }
             }
-            public float weightedSum()
+            float weightedSum()
             {
                 float one = Synapses[1].Weight * Inputs[1];
+                double done = (double)one;
+                Synapses[1].Percentage = (float) sigmoid(done);
                 float two = Synapses[2].Weight * Inputs[2];
+                double dtwo = (double)two;
+                Synapses[2].Percentage = (float) sigmoid(dtwo);
                 float three = Synapses[3].Weight * Inputs[3];
+                double dthree = (double)three;
+                Synapses[3].Percentage = (float) sigmoid(dthree);
                 return one + two + three;
             }
             double sigmoid(double x)
@@ -40,13 +47,41 @@ namespace cstest
                 return 2 / (1 + Math.Exp(-2 * x)) - 1;
             }
         }
+        class Train
+        {
+            float nDiff;
+            float chVal;
+            public void train()
+            {
+                for(int i = 1; i > 3; i++)
+                {
+                    if(Outputs[i] == 1)
+                    {
+                        nDiff = 1 - Synapses[i].Percentage;
+                        chVal = Synapses[i].Difference - nDiff;
+                    }
+                    else
+                    {
+                        nDiff = Synapses[1].Percentage;
+                        chVal = Synapses[i].Difference - nDiff;
+                    }
+                    float perVal = 1 + chVal;
+                    Synapses[i].Weight *= perVal;
+                    Synapses[i].Difference = nDiff;
+                }
+            }
+        }
         public class Init
         {
+            Predict P = new Predict();
+            Train T = new Train();
             public void init(int weight, string inputs)
             {
                 Synapses = new Synapse[3]{ new Synapse(weight), new Synapse(weight), new Synapse(weight) };
                 string[] splitputs = inputs.Split(' ');
                 Inputs = Array.ConvertAll(splitputs, new Converter<string, int>(convert));
+                P.p();
+                T.train();
             }
             public void init(int[] inputs)
             {
@@ -61,14 +96,10 @@ namespace cstest
         {
             public float Weight;
             public float Percentage;
+            public float Difference = 1;
             public Synapse(float weight)
             {
                 Weight = weight;
-            }
-            public Synapse(float change, float percentage)
-            {
-                Weight += change;
-                Percentage = percentage;
             }
         }
     }
@@ -76,7 +107,10 @@ namespace cstest
     {
         static void Main(string[] args)
         {
+            NeuralNet.Init nni = new NeuralNet.Init();
+            nni.init(1, "1 0 1");
 
+            
         }
     }
 }
